1285d6e487dd58bd7e6d888e139f1af8de2c461d
ultimania <fukaya-takumi@sysj.co.jp>
2024-02-12 12:10:59 +0900

    fix: fix meta info

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 #require_dependency 'redmine_budget_hook_listener'
 
 Redmine::Plugin.register :redmine_budget do
-  name 'Polls plugin'
-  author 'Author name'
+  name 'Budget Management plugin'
+  author 'Takumi Fukaya'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/ultimania/redmine_budget'
+  author_url 'https://github.com/ultimania'
   menu :application_menu, :redmine_budget, { :controller => 'redmine_budget', :action => 'index' }, :caption => 'Polls'
   # permission :redmine_budget, { :redmine_budget => [:index, :vote] }, :public => true
   # permission :view_redmine_budget, :redmine_budget => :index
