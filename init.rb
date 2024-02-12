bbf0b9b5ef732e466e9f09472662fc295ef5af82
ultimania <fukaya-takumi@sysj.co.jp>
2024-02-12 12:09:52 +0900

    fix: fix plugin name

--- init.rb
+++ init.rb
@@ -1,19 +1,19 @@
-#require_dependency 'polls_hook_listener'
+#require_dependency 'redmine_budget_hook_listener'
 
-Redmine::Plugin.register :polls do
+Redmine::Plugin.register :redmine_budget do
   name 'Polls plugin'
   author 'Author name'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
-  menu :application_menu, :polls, { :controller => 'polls', :action => 'index' }, :caption => 'Polls'
-  # permission :polls, { :polls => [:index, :vote] }, :public => true
-  # permission :view_polls, :polls => :index
-  # permission :vote_polls, :polls => :vote
-  project_module :polls do
-    permission :view_polls, :polls => :index
-    permission :vote_polls, :polls => :vote
+  menu :application_menu, :redmine_budget, { :controller => 'redmine_budget', :action => 'index' }, :caption => 'Polls'
+  # permission :redmine_budget, { :redmine_budget => [:index, :vote] }, :public => true
+  # permission :view_redmine_budget, :redmine_budget => :index
+  # permission :vote_redmine_budget, :redmine_budget => :vote
+  project_module :redmine_budget do
+    permission :view_redmine_budget, :redmine_budget => :index
+    permission :vote_redmine_budget, :redmine_budget => :vote
   end
-  menu :project_menu, :polls, { :controller => 'polls', :action => 'index' }, :caption => 'Polls', :after => :activity, :param => :project_id
+  menu :project_menu, :redmine_budget, { :controller => 'redmine_budget', :action => 'index' }, :caption => 'Polls', :after => :activity, :param => :project_id
 end
